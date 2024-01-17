294a1faf2befeb117d46e1c292aa5eb820e2dd6a
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-01-17 13:45:22 -0700

    Add project files.

---
+++ SayMyName/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SayMyName")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("SayMyName")>
+<Assembly: AssemblyCopyright("Copyright © HP 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f7a48ee4-a894-4e41-8613-8f8bf3f58e1c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,15 @@
+﻿Module SayMyName
+
+    Sub Main()
+        Dim userInput As String
+
+        Console.WriteLine("What is your name?")
+
+        userInput = Console.ReadLine()
+
+        Console.WriteLine($"Hello, {userInput}! Nice to meet you.")
+
+        Console.Read()
+    End Sub
+
+End Module
